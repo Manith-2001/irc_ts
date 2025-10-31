7f023fd569f1adc215a8ef3aeb605d4073eb3dd3
Manith-2001 <manith.vazirani@pec.edu>
2025-11-01 00:52:25 +0530

    added : sending commands to main server

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
 import * as net from "net";
+import * as readline from "node:readline";
+import { stdin, stdout } from "node:process";
 const PORT = 6665;
 const HOST = "irc.libera.chat";
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+function ask() {
+  rl.question("Enter your command: ", (answer) => {
+    const command = answer + '\r\n';
+    const raw = Buffer.from(command, "ascii");
+    console.log(raw.toString('hex'));
+    client.write(command);
+    ask();
+  });
+}
 
 var client = net.connect(
   {
@@ -13,13 +26,14 @@ var client = net.connect(
     client.write("NICK gx_ultra\r\n");
     client.write("USER gxultra 0 * :GXULTRA\r\n");
     client.write("CAP END\r\n");
+    ask();
   },
 );
 
 client.on("data", (data) => {
   console.log(data.toString("utf8"));
-	if(data.includes("PING :")){
-		const token = data.toString().replace("PING :", "")
-		client.write(`PONG ${token}\r\n`)
-	}
+  if (data.includes("PING :")) {
+    const token = data.toString().replace("PING :", "");
+    client.write(`PONG ${token}\r\n`);
+  }
 });
