00459bcfb4c3d70b460f59b85df813ff1af2ce30
Manith-2001 <manith.vazirani@pec.edu>
2025-11-02 01:48:42 +0530

    changed : to class structure and event emitters

--- index.ts
+++ index.ts
@@ -1,29 +1,60 @@
-// import * as net from "net";
 import net = require("net");
+import EventEmitter = require("events");
 
-interface ClientProps {
-  server: string;
-  port: number;
-  nick: string;
-  username: string;
-  realname: string;
+interface Client {
+	server: string;
+	port: number;
+	nick: string;
+	username: string;
+	realname: string;
 }
 
-function IRCClient({ server, port, nick, username, realname }: ClientProps) {
-  var client = net.connect(
-    {
-      host: server,
-      port: port,
-    },
-    () => {
-      client.write("CAP LS 302\r\n");
-      client.write("PASS test123\r\n");
-      client.write(`NICK ${nick}\r\n`);
-      client.write(`USER ${username} 0 * :${realname}\r\n`);
-      client.write("CAP END\r\n");
-    },
-  );
-  return client;
+class IRCClient implements Client {
+	server: string;
+	port: number;
+	nick: string;
+	username: string;
+	realname: string;
+	client: any;
+	myEmitter: EventEmitter;
+
+	constructor(
+		server: string,
+		port: number,
+		nick: string,
+		username: string,
+		realname: string,
+	) {
+		this.server = server;
+		this.port = port;
+		this.nick = nick;
+		this.username = username;
+		this.realname = realname;
+		this.myEmitter = new EventEmitter();
+
+		this.client = net.connect(
+			{
+				host: server,
+				port: port,
+			},
+			() => {
+				this.client.write("CAP LS 302\r\n");
+				this.client.write("PASS test123\r\n");
+				this.client.write(`NICK ${nick}\r\n`);
+				this.client.write(`USER ${username} 0 * :${realname}\r\n`);
+				this.client.write("CAP END\r\n");
+			},
+		);
+		this.client.on("data", (data: any) => {
+			if (data.includes("PING :")) {
+				const token = data.toString().replace("PING :", "");
+				this.client.write(`PONG ${token}\r\n`);
+			}
+			else {
+				this.myEmitter.emit("data", data);
+			}
+		});
+	}
 }
 
-export = { IRCClient }
+export = { IRCClient };
