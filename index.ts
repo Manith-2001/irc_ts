df37cb4717c6cd9ad286b3f9bfe8d4b467dbbc6f
Manith-2001 <manith.vazirani@pec.edu>
2025-11-01 22:35:48 +0530

    changed : made the library to export function and test in another local project

--- index.ts
+++ index.ts
@@ -1,39 +1,32 @@
 import * as net from "net";
-import * as readline from "node:readline";
-import { stdin, stdout } from "node:process";
-const PORT = 6665;
-const HOST = "irc.libera.chat";
-const rl = readline.createInterface({ input: stdin, output: stdout });
 
-function ask() {
-  rl.question("Enter your command: ", (answer) => {
-    const command = answer + '\r\n';
-    const raw = Buffer.from(command, "ascii");
-    console.log(raw.toString('hex'));
-    client.write(command);
-    ask();
-  });
+interface ClientProps {
+  server: string;
+  port: number;
+  nick: string;
+  username: string;
+  realname: string;
 }
 
-var client = net.connect(
-  {
-    host: HOST,
-    port: PORT,
-  },
-  () => {
-    client.write("CAP LS 302\r\n");
-    client.write("PASS test123\r\n");
-    client.write("NICK gx_ultra\r\n");
-    client.write("USER gxultra 0 * :GXULTRA\r\n");
-    client.write("CAP END\r\n");
-    ask();
-  },
-);
-
-client.on("data", (data) => {
-  console.log(data.toString("utf8"));
-  if (data.includes("PING :")) {
-    const token = data.toString().replace("PING :", "");
-    client.write(`PONG ${token}\r\n`);
-  }
-});
+export function IRCClient({
+  server,
+  port,
+  nick,
+  username,
+  realname,
+}: ClientProps) {
+  var client = net.connect(
+    {
+      host: server,
+      port: port,
+    },
+    () => {
+      client.write("CAP LS 302\r\n");
+      client.write("PASS test123\r\n");
+      client.write(`NICK ${nick}\r\n`);
+      client.write(`USER ${username} 0 * :${realname}\r\n`);
+      client.write("CAP END\r\n");
+    },
+  );
+  return client;
+}
